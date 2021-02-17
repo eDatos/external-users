584608b4e7a29f94128fc3502a56f8e7d0f60ffc
nunigs <ngonsot@arte-consultores.com>
2021-02-17 10:16:04 +0000

    EDATOS-3266 - Configurar entorno de desarrollo local y crear el proyecto base > Se elimina el módulo de **-external-api al no ser necesario para external-external > Se eliminan las dependencias de **-external-api del proyecto > Se añaden las dependencias al pom -external-web

--- edatos-external-users-external-api/src/main/java/es/gobcan/istac/edatos/external/users/external/api/invocation/CommonMetadataRestExternalFacade.java
+++
@@ -1,8 +0,0 @@
-package es.gobcan.istac.edatos.external.users.external.api.invocation;
-
-import org.siemac.metamac.rest.common_metadata.v1_0.domain.Configuration;
-
-public interface CommonMetadataRestExternalFacade {
-
-    public Configuration retrieveConfigurationById(String id);
-}
