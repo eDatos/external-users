7dc5be2c641672ce6239a262c6548ef25f038dad
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-10 13:25:15 +0000

    EDATOS-3294 - CRUD favoritos
    > Elimina mensajes no utilizados de ServiceExceptionParameters y
    ServiceExceptionType.
    > Elimina mensajes no utilizados de
    messages-external_users.properties,
    messages-external_users_en.properties y
    messages-external_users_es.properties

--- edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/errors/ServiceExceptionParameters.java
+++ edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/errors/ServiceExceptionParameters.java
@@ -1,18 +1,6 @@
 package es.gobcan.istac.edatos.external.users.core.errors;
 
-public class ServiceExceptionParameters
-    extends org.siemac.edatos.core.common.exception.CommonServiceExceptionParameters {
-
-    public static final String INSTANCES_ID_LIST_SIZE = "parameter.external_users.intances_id_list.size";
-
-    public static final String FAMILY_TITLE = "parameter.external_users.family.title";
-    public static final String FAMILY_CODE = "parameter.external_users.family.code";
-    public static final String FAMILY_URN = "parameter.external_users.family.urn";
-    public static final String FAMILY_ACRONYM = "parameter.external_users.family.acronym";
-    public static final String FAMILY_DESCRIPTION = "parameter.external_users.family.description";
-    public static final String FAMILY_PROC_STATUS = "parameter.external_users.family.proc_status";
-    public static final String FAMILY_INTERNAL_INVENTORY_DATE = "parameter.external_users.family.internal_inventory_date";
-    public static final String FAMILY_INVENTORY_DATE = "parameter.external_users.family.inventory_date";
+public class ServiceExceptionParameters extends org.siemac.edatos.core.common.exception.CommonServiceExceptionParameters {
 
     public static final String OPERATION_CODE = "parameter.external_users.operation.code";
     public static final String OPERATION_URN = "parameter.external_users.operation.urn";
@@ -24,15 +12,9 @@ public class ServiceExceptionParameters
     public static final String OPERATION_STATUS = "parameter.external_users.operation.status";
     public static final String OPERATION_SUBJECT_AREA = "parameter.external_users.operation.subject_area";
     public static final String OPERATION_SECONDARY_SUBJECT_AREAS = "parameter.external_users.operation.secondary_subject_areas";
-    public static final String OPERATION_SURVEY_TYPE = "parameter.external_users.operation.survey_type";
     public static final String OPERATION_INDICATOR_SYSTEM = "parameter.external_users.operation.indicator_system";
-    public static final String OPERATION_RELEASE_CALENDAR_ACCESS = "parameter.external_users.operation.release_calendar_access";
-    public static final String OPERATION_REL_POL_US_AC = "parameter.external_users.operation.rel_pol_us_ac";
-    public static final String OPERATION_REV_POLICY = "parameter.external_users.operation.rev_policy";
-    public static final String OPERATION_REV_PRACTICE = "parameter.external_users.operation.rev_practice";
     public static final String OPERATION_COMMENT = "parameter.external_users.operation.comment";
     public static final String OPERATION_NOTES = "parameter.external_users.operation.notes";
-    public static final String OPERATION_OFFICIALITY_TYPE = "parameter.external_users.operation.officiality_type";
     public static final String OPERATION_PRODUCER = "parameter.external_users.operation.producer";
     public static final String OPERATION_REGIONAL_RESPONSIBLE = "parameter.external_users.operation.regional_responsible";
     public static final String OPERATION_REGIONAL_CONTRIBUTOR = "parameter.external_users.operation.regional_contributor";
@@ -42,59 +24,4 @@ public class ServiceExceptionParameters
     public static final String OPERATION_INTERNAL_INVENTORY_DATE = "parameter.external_users.operation.internal_inventory_date";
     public static final String OPERATION_INVENTORY_DATE = "parameter.external_users.operation.inventory_date";
     public static final String OPERATION_COMMON_METADATA = "parameter.external_users.operation.common_metadata";
-    public static final String OPERATION_SPECIFIC_LEGAL_ACTS = "parameter.external_users.operation.specific_legal_acts";
-    public static final String OPERATION_SPECIFIC_DATA_SHARING = "parameter.external_users.operation.specific_data_sharing";
-
-    public static final String INSTANCE_CODE = "parameter.external_users.instance.code";
-    public static final String INSTANCE_URN = "parameter.external_users.instance.urn";
-    public static final String INSTANCE_TITLE = "parameter.external_users.instance.title";
-    public static final String INSTANCE_ACRONYM = "parameter.external_users.instance.acronym";
-    public static final String INSTANCE_DESCRIPTION = "parameter.external_users.instance.description";
-    public static final String INSTANCE_STATISTICAL_POPULATION = "parameter.external_users.instance.statistical_population";
-    public static final String INSTANCE_STATISTICAL_UNIT = "parameter.external_users.instance.statistical_unit";
-    public static final String INSTANCE_GEOGRAPHIC_GRANULARITY = "parameter.external_users.instance.geographic_granularity";
-    public static final String INSTANCE_GEOGRAPHIC_COMPARABILITY = "parameter.external_users.instance.geographic_comparability";
-    public static final String INSTANCE_TEMPORAL_GRANULARITY = "parameter.external_users.instance.temporal_granularity";
-    public static final String INSTANCE_TEMPORAL_COMPARABILITY = "parameter.external_users.instance.temporal_comparability";
-    public static final String INSTANCE_UNIT_MEASURE = "parameter.external_users.instance.unit_measure";
-    public static final String INSTANCE_STAT_CONC_DEF = "parameter.external_users.instance.stat_conc_def";
-    public static final String INSTANCE_STAT_CONC_DEF_LIST = "parameter.external_users.instance.stat_conc_def_list";
-    public static final String INSTANCE_CLASS_SYSTEM = "parameter.external_users.instance.class_system";
-    public static final String INSTANCE_CLASS_SYSTEM_LIST = "parameter.external_users.instance.class_system_list";
-    public static final String INSTANCE_DOC_METHOD = "parameter.external_users.instance.doc_method";
-    public static final String INSTANCE_INFORMATION_SUPPLIERS = "parameter.external_users.instance.information_suppliers";
-    public static final String INSTANCE_FREQ_COLL = "parameter.external_users.instance.freq_coll";
-    public static final String INSTANCE_DATA_VALIDATION = "parameter.external_users.instance.data_validation";
-    public static final String INSTANCE_DATA_COMPILATION = "parameter.external_users.instance.data_compilation";
-    public static final String INSTANCE_ADJUSTMENT = "parameter.external_users.instance.adjustment";
-    public static final String INSTANCE_COST_BURDEN = "parameter.external_users.instance.cost_burden";
-    public static final String INSTANCE_QUALITY_DOC = "parameter.external_users.instance.quality_doc";
-    public static final String INSTANCE_QUALITY_ASSURE = "parameter.external_users.instance.quality_assure";
-    public static final String INSTANCE_QUALITY_ASSMNT = "parameter.external_users.instance.quality_assmnt";
-    public static final String INSTANCE_USER_NEEDS = "parameter.external_users.instance.user_needs";
-    public static final String INSTANCE_USER_SAT = "parameter.external_users.instance.user_sat";
-    public static final String INSTANCE_COMPLETENESS = "parameter.external_users.instance.completeness";
-    public static final String INSTANCE_TIMELINESS = "parameter.external_users.instance.timeliness";
-    public static final String INSTANCE_PUNCTUALITY = "parameter.external_users.instance.punctuality";
-    public static final String INSTANCE_ACCURACY_OVERALL = "parameter.external_users.instance.accuracy_overall";
-    public static final String INSTANCE_SAMPLING_ERR = "parameter.external_users.instance.sampling_err";
-    public static final String INSTANCE_NONSAMPLING_ERR = "parameter.external_users.instance.nonsampling_err";
-    public static final String INSTANCE_COHER_X_DOMAIN = "parameter.external_users.instance.coher_x_domain";
-    public static final String INSTANCE_COHER_INTERNAL = "parameter.external_users.instance.coher_internal";
-    public static final String INSTANCE_COMMENT = "parameter.external_users.instance.comment";
-    public static final String INSTANCE_NOTES = "parameter.external_users.instance.notes";
-    public static final String INSTANCE_ORDER = "parameter.external_users.instance.order";
-    public static final String INSTANCE_PROC_STATUS = "parameter.external_users.instance.proc_status";
-    public static final String INSTANCE_INSTANCE_TYPE = "parameter.external_users.instance.instance_type";
-    public static final String INSTANCE_INTERNAL_INVENTORY_DATE = "parameter.external_users.instance.internal_inventory_date";
-    public static final String INSTANCE_INVENTORY_DATE = "parameter.external_users.instance.inventory_date";
-    public static final String INSTANCE_BASE_PERIOD = "parameter.external_users.instance.base_period";
-
-    // Needs
-    public static final String NEED = "parameter.external_users.need";
-    public static final String NEED_CODE = "parameter.external_users.need.code";
-    public static final String NEED_URN = "parameter.external_users.need.urn";
-    public static final String NEED_TITLE = "parameter.external_users.need.title";
-    public static final String NEED_PROC_STATUS = "parameter.external_users.need.proc_status";
-
 }
