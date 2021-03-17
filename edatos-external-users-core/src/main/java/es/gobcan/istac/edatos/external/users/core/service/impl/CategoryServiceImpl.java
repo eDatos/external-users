920bb8ba6b2d7b03c520516d8b1b468cfefef1e1
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-17 14:13:16 +0000

    EDATOS-3294 - CRUD favoritos
    > Elimina funciones no usadas de CategoryService.
    > Mueve el lanzamiento de excepciones de CategoryService los respectivos
    controladores.
    > Hace que CategoryEntity#code sea único, y elimina esta validación del
    servicio.

--- edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/service/impl/CategoryServiceImpl.java
+++ edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/service/impl/CategoryServiceImpl.java
@@ -3,13 +3,11 @@ package es.gobcan.istac.edatos.external.users.core.service.impl;
 import java.util.List;
 
 import org.hibernate.criterion.DetachedCriteria;
-import org.siemac.edatos.core.common.exception.EDatosException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import es.gobcan.istac.edatos.external.users.core.domain.CategoryEntity;
-import es.gobcan.istac.edatos.external.users.core.errors.ServiceExceptionType;
 import es.gobcan.istac.edatos.external.users.core.repository.CategoryRepository;
 import es.gobcan.istac.edatos.external.users.core.service.CategoryService;
 import es.gobcan.istac.edatos.external.users.core.util.QueryUtil;
@@ -27,29 +25,7 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryEntity findCategoryById(Long id) {
-        CategoryEntity category = categoryRepository.findOne(id);
-        if (category == null) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_NOT_FOUND);
-        }
-        return category;
-    }
-
-    @Override
-    public CategoryEntity findCategoryByCode(String code) {
-        CategoryEntity category = categoryRepository.findByCode(code);
-        if (category == null) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_CODE_NOT_FOUND, code);
-        }
-        return category;
-    }
-
-    @Override
-    public CategoryEntity findCategoryByUrn(String urn) {
-        CategoryEntity category = categoryRepository.findByUrn(urn);
-        if (category == null) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_NOT_FOUND, urn);
-        }
-        return category;
+        return categoryRepository.findOne(id);
     }
 
     @Override
@@ -70,24 +46,16 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public CategoryEntity createCategory(CategoryEntity category) {
-        validateCategoryCodeUnique(category.getCode(), null);
         return categoryRepository.saveAndFlush(category);
     }
 
     @Override
     public CategoryEntity updateCategory(CategoryEntity category) {
-        validateCategoryCodeUnique(category.getCode(), category.getId());
         return categoryRepository.saveAndFlush(category);
     }
 
     @Override
     public void deleteCategory(Long categoryId) {
         categoryRepository.delete(categoryId);
-    }
-
-    private void validateCategoryCodeUnique(String code, Long actualId) {
-        if ((actualId == null && categoryRepository.existsByCode(code)) || (actualId != null && categoryRepository.existsByCodeAndIdNot(code, actualId))) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_ALREADY_EXIST_CODE_DUPLICATED, code);
-        }
     }
 }
