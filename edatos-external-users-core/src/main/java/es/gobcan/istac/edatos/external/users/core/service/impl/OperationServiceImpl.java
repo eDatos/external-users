593230f93d2bc8eb815e9c4a8521c25bf9b0b3af
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-17 14:16:51 +0000

    EDATOS-3294 - CRUD favoritos
    > Elimina funciones no usadas de OperationService.
    > Mueve el lanzamiento de excepciones de OperationService los
    respectivos controladores.
    > Hace que OperationEntity#code sea único, y elimina esta validación del
    servicio.

--- edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/service/impl/OperationServiceImpl.java
+++ edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/service/impl/OperationServiceImpl.java
@@ -1,19 +1,13 @@
 package es.gobcan.istac.edatos.external.users.core.service.impl;
 
-import java.time.Instant;
 import java.util.List;
 
 import org.hibernate.criterion.DetachedCriteria;
-import org.siemac.edatos.core.common.exception.EDatosException;
-import org.siemac.edatos.core.common.util.GeneratorUrnUtils;
-import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.ProcStatus;
-import org.siemac.metamac.rest.statistical_operations_internal.v1_0.domain.Status;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import es.gobcan.istac.edatos.external.users.core.domain.OperationEntity;
-import es.gobcan.istac.edatos.external.users.core.errors.ServiceExceptionType;
 import es.gobcan.istac.edatos.external.users.core.repository.OperationRepository;
 import es.gobcan.istac.edatos.external.users.core.service.OperationService;
 import es.gobcan.istac.edatos.external.users.core.util.QueryUtil;
@@ -31,29 +25,7 @@ public class OperationServiceImpl implements OperationService {
 
     @Override
     public OperationEntity findOperationById(Long id) {
-        OperationEntity operation = operationRepository.findOne(id);
-        if (operation == null) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_NOT_FOUND);
-        }
-        return operation;
-    }
-
-    @Override
-    public OperationEntity findOperationByCode(String code) {
-        OperationEntity operation = operationRepository.findByCode(code);
-        if (operation == null) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_CODE_NOT_FOUND, code);
-        }
-        return operation;
-    }
-
-    @Override
-    public OperationEntity findOperationByUrn(String urn) {
-        OperationEntity operation = operationRepository.findByUrn(urn);
-        if (operation == null) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_NOT_FOUND, urn);
-        }
-        return operation;
+        return operationRepository.findOne(id);
     }
 
     @Override
@@ -74,9 +46,6 @@ public class OperationServiceImpl implements OperationService {
 
     @Override
     public OperationEntity createOperation(OperationEntity operation) {
-        operation.setUrn(GeneratorUrnUtils.generateSiemacStatisticalOperationUrn(operation.getCode()));
-        operation.setStatus(Status.PLANNING);
-        validateOperationCodeUnique(operation.getCode(), null);
         return operationRepository.save(operation);
     }
 
@@ -91,25 +60,4 @@ public class OperationServiceImpl implements OperationService {
         operationRepository.delete(operation);
     }
 
-    @Override
-    public OperationEntity publishInternallyOperation(Long id) {
-        OperationEntity operation = findOperationById(id);
-        operation.setProcStatus(ProcStatus.INTERNALLY_PUBLISHED);
-        operation.setInternalInventoryDate(Instant.now());
-        return updateOperation(operation);
-    }
-
-    @Override
-    public OperationEntity publishExternallyOperation(Long id) {
-        OperationEntity operation = findOperationById(id);
-        operation.setProcStatus(ProcStatus.EXTERNALLY_PUBLISHED);
-        operation.setInventoryDate(Instant.now());
-        return updateOperation(operation);
-    }
-
-    private void validateOperationCodeUnique(String code, Long actualId) {
-        if ((actualId == null && operationRepository.existsByCode(code)) || (actualId != null && operationRepository.existsByCodeAndIdNot(code, actualId))) {
-            throw new EDatosException(ServiceExceptionType.OPERATION_ALREADY_EXIST_CODE_DUPLICATED, code);
-        }
-    }
 }
