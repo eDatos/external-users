f9a8c7535ad41ffd1749278b991f022bc027a83e
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-23 10:46:11 +0000

    EDATOS-3294 - CRUD favoritos
    > Corrige el uso de Filter en el resolver de Favoritos.

--- edatos-external-users-internal-js/webapp/src/app/modules/favorite/favorite.resolver.ts
+++ edatos-external-users-internal-js/webapp/src/app/modules/favorite/favorite.resolver.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Filter, FilterService } from '@app/shared';
+import { Favorite, FavoriteService } from '@app/shared';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
-export class FavoriteResolver implements Resolve<Filter> {
-    constructor(private filterService: FilterService) {}
+export class FavoriteResolver implements Resolve<Favorite> {
+    constructor(private filterService: FavoriteService) {}
 
-    public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Filter> {
+    public resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Favorite> {
         const filterId = (route.paramMap.get('id') as unknown) as number;
         return this.filterService.get(filterId);
     }
