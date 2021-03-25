fd53e0bddcda2fa40c138347cfa1ec74cd60d48b
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-25 07:24:30 +0000

    EDATOS-3294 - CRUD favoritos
    > Añade funciones a response-utils.ts que permiten simplificar el mapeo
    de los modelos en los servicios.

--- edatos-external-users-internal-js/webapp/src/app/core/utils/response-utils.ts
+++ edatos-external-users-internal-js/webapp/src/app/core/utils/response-utils.ts
@@ -1,13 +1,23 @@
 import { HttpResponse } from '@angular/common/http';
 import { ResponseWrapper } from 'arte-ng/model';
 import { plainToClass } from 'class-transformer';
+import { OperatorFunction } from 'rxjs';
+import { map } from 'rxjs/operators';
 
-export function convert<T>(element: T, clazz: new () => T) {
+export function mapPlainToResponseWrapper<T>(clazz: new () => T): OperatorFunction<HttpResponse<T>, ResponseWrapper> {
+    return map((response) => convertToResponseWrapper(response, clazz));
+}
+
+export function mapPlainToClass<T>(clazz: new () => T): OperatorFunction<T, T> {
+    return map((element) => convert(element, clazz));
+}
+
+export function convert<T>(element: T, clazz: new () => T): T {
     return plainToClass(clazz, element);
 }
 
-export function convertToResponseWrapper<T>(response: HttpResponse<any>, clazz: new () => T) {
+export function convertToResponseWrapper<T>(response: HttpResponse<T>, clazz: new () => T) {
     const { headers, body, status } = response;
-    const data = Array.isArray(body) ? body.map((element) => convert(element, clazz)) : this.convert(body);
+    const data = Array.isArray(body) ? body.map((element) => convert(element, clazz)) : convert(body, clazz);
     return new ResponseWrapper(headers, data, status);
 }
--- edatos-external-users-internal-js/webapp/src/app/shared/service/favorite/favorite.service.ts
+++ edatos-external-users-internal-js/webapp/src/app/shared/service/favorite/favorite.service.ts
@@ -1,12 +1,10 @@
-import { HttpClient, HttpResponse } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { convert, convertToResponseWrapper } from '@app/core/utils/response-utils';
+import { mapPlainToClass, mapPlainToResponseWrapper } from '@app/core/utils/response-utils';
 import { Favorite } from '@app/shared/model/favorite.model';
 import { ResponseWrapper } from 'arte-ng/model';
 import { createRequestOption } from 'arte-ng/utils';
-import { plainToClass } from 'class-transformer';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -17,19 +15,21 @@ export class FavoriteService {
     constructor(private http: HttpClient) {}
 
     public get(id: number): Observable<Favorite> {
-        return this.http.get<Favorite>(`${this.resourceUrl}/${id}`).pipe(map((favorite) => convert(favorite, Favorite)));
+        return this.http.get<Favorite>(`${this.resourceUrl}/${id}`).pipe(mapPlainToClass(Favorite));
     }
 
     public find(req?: any): Observable<ResponseWrapper> {
         const options = createRequestOption(req);
-        return this.http.get(this.resourceUrl, { ...options, observe: 'response' }).pipe(map((response) => convertToResponseWrapper(response, Favorite)));
+        return this.http
+            .get<Favorite>(this.resourceUrl, { ...options, observe: 'response' })
+            .pipe(mapPlainToResponseWrapper(Favorite));
     }
 
     public save(filter: Favorite): Observable<Favorite> {
-        return this.http.post<Favorite>(this.resourceUrl, filter).pipe(map((favorite) => convert(favorite, Favorite)));
+        return this.http.post<Favorite>(this.resourceUrl, filter).pipe(mapPlainToClass(Favorite));
     }
 
     public update(filter: Favorite): Observable<Favorite> {
-        return this.http.put<Favorite>(this.resourceUrl, filter).pipe(map((favorite) => convert(favorite, Favorite)));
+        return this.http.put<Favorite>(this.resourceUrl, filter).pipe(mapPlainToClass(Favorite));
     }
 }
