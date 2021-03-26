6e931c12873c721872b88b538ea7419e79c02614
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-26 11:38:12 +0000

    EDATOS-3294 - CRUD favoritos
    > Suprime la función mapPlainToClass debido a problemas de tipado.
    Parece ser que el alguna parte (seguramente a través de
    OperatorFunction) el tipado de los arrays se pierde y Typescript
    detecta un falso error de compilación (pese a que el código es
    correcto).

--- edatos-external-users-internal-js/webapp/src/app/core/utils/response-utils.ts
+++ edatos-external-users-internal-js/webapp/src/app/core/utils/response-utils.ts
@@ -1,23 +1,116 @@
 import { HttpHeaders, HttpResponse } from '@angular/common/http';
 import { plainToClass } from 'class-transformer';
-import { OperatorFunction } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 export class ResponseWrapper<T> {
     constructor(public headers: HttpHeaders, public body: T, public status: number) {}
 }
 
-export function mapPlainToClass<T>(clazz: new () => T): OperatorFunction<T, T>;
-export function mapPlainToClass<T>(clazz: new () => T): OperatorFunction<HttpResponse<T>, ResponseWrapper<T>>;
-export function mapPlainToClass<T>(clazz: new () => T): OperatorFunction<HttpResponse<T[]>, ResponseWrapper<T[]>>;
-export function mapPlainToClass<T>(clazz: new () => T): OperatorFunction<T | T[] | HttpResponse<T>, T | T[] | ResponseWrapper<T>> {
-    return map((obj) => convert(clazz, obj));
-}
+/**
+ * Converts a plain object into an instance of a class,
+ * The class has to mark with `@Type` its fields that are meant to be instantiated with data
+ * from the plain object. For more info about this, please refer to
+ * {@link https://github.com/typestack/class-transformer#working-with-nested-objects}.
+ *
+ * @param clazz the constructor of the class.
+ * @param plain the object to be converted.
+ * @return an instance of the class filled with the data from the plain object, including
+ * nested fields to an arbitrary level if they were annotated in the class with `@Type`.
+ *
+ * @example
+ * ```
+ * class MySubclass {
+ *     anotherKey: number;
+ * }
+ *
+ * class MyClass {
+ *     key: string;
+ *
+ *     @Type(() => MySubclass)
+ *     nested: MySubclass;
+ * }
+ *
+ * const json = {
+ *     key: "value",
+ *     nested: {
+ *         anotherKey: 3,
+ *     }
+ * };
+ *
+ * const instantiatedObject = convert(MyClass, json);
+ * console.log(instantiatedObject instanceof MyClass); // true
+ * console.log(instantiatedObject.nested instanceof MySubclass); // true
+ * ```
+ */
+export function convert<T>(clazz: { new (): T }, plain: T): T;
 
-export function convert<T>(clazz: new () => T, plain: any): T;
-export function convert<T>(clazz: new () => T, plain: HttpResponse<any>): ResponseWrapper<T>;
-export function convert<T>(clazz: new () => T, plain: HttpResponse<any[]>): ResponseWrapper<T[]>;
-export function convert<T>(clazz: new () => T, obj: any | any[] | HttpResponse<any | any[]>): T | T[] | ResponseWrapper<T | T[]> {
+/**
+ * Converts every plain object from the array into an instance of a class,.
+ * The class has to mark with `@Type` its fields that are meant to be instantiated with data
+ * from the plain object. For more info about this, please refer to
+ * {@link https://github.com/typestack/class-transformer#working-with-nested-objects}.
+ *
+ * @param clazz the constructor of the class.
+ * @param arrayOfPlains the array of objects to be converted.
+ * @return an array with instances of the class filled with the data from the plain objects, including
+ * nested fields to an arbitrary level if they were annotated in the class with `@Type`.
+ */
+export function convert<T>(clazz: { new (): T }, arrayOfPlains: T[]): T[];
+
+/**
+ * Converts a plain object that is contained in the HttpResponse into an instance of a class, and returns
+ * the converted object wrapped in a ResponseWrapper.
+ * The class to which the object is going to be converted has to mark with `@Type` the fields that are meant to be
+ * instantiated, otherwise only the main object will be instantiated.
+ * For more info about this, please refer to
+ * {@link https://github.com/typestack/class-transformer#working-with-nested-objects}.
+
+ * @param clazz the constructor of the class.
+ * @param response the response provided by the HttpClient.
+ * @return a ResponseWrapper containing the instantiated objects.
+ *
+ * @remarks This function is meant to be used with the HttpClient, when the request does come
+ * in the form of a HttpResponse and not the body directly (for example, when the request
+ * is made with `{observe: 'response'}`. See the example below.
+ *
+ * @example
+ * ```
+ * public find(id: number): Observable<ResponseWrapper<Favorite>> {
+ *     return this.http
+ *         .get<Favorite>(`${this.resourceUrl}/${id}`, { observe: 'response' })
+ *         .pipe(map((httpResponse: HttpResponse<Favorite>) => convert(Favorite, httpResponse)));
+ * }
+ * ```
+ */
+export function convert<T>(clazz: { new (): T }, response: HttpResponse<T>): ResponseWrapper<T>;
+
+/**
+ * Converts an array of plain objects that are contained in the HttpResponse into instances of a class, and returns
+ * the array of converted objects wrapped in a ResponseWrapper.
+ * The class to which the objects are going to be converted has to mark with `@Type` the fields that are meant to be
+ * instantiated, otherwise only the main object will be instantiated.
+ * For more info about this, please refer to
+ * {@link https://github.com/typestack/class-transformer#working-with-nested-objects}.
+
+ * @param clazz the constructor of the class.
+ * @param response the response provided by the HttpClient.
+ * @return a ResponseWrapper containing the array of instantiated objects.
+ *
+ * @remarks This function is meant to be used with the HttpClient, when the request does come
+ * in the form of a HttpResponse and not the body directly (for example, when the request
+ * is made with `{observe: 'response'}`. See the example below.
+ *
+ * @example
+ * ```
+ * public findAll(): Observable<ResponseWrapper<Favorite[]>> {
+ *     return this.http
+ *         .get<Favorite[]>(this.resourceUrl, { observe: 'response' })
+ *         .pipe(map((httpResponse: HttpResponse<Favorite[]>) => convert(Favorite, httpResponse)));
+ * }
+ * ```
+ */
+export function convert<T>(clazz: { new (): T }, response: HttpResponse<T[]>): ResponseWrapper<T[]>;
+
+export function convert<T>(clazz: { new (): T }, obj: T | T[] | HttpResponse<T | T[]>): T | T[] | ResponseWrapper<T | T[]> {
     if (Array.isArray(obj)) {
         return convertToList(clazz, obj);
     }
@@ -31,7 +124,7 @@ function convertToList<T>(clazz: { new (): T }, list: any[]): T[] {
     return list.map((element) => plainToClass(clazz, element));
 }
 
-function convertToResponseWrapper<T>(clazz: new () => T, response: HttpResponse<T | T[]>): ResponseWrapper<T | T[]> {
+function convertToResponseWrapper<T>(clazz: { new (): T }, response: HttpResponse<T | T[]>): ResponseWrapper<T | T[]> {
     const { headers, body, status } = response;
     const data = Array.isArray(body) ? convertToList(clazz, body) : plainToClass(clazz, body);
     return new ResponseWrapper(headers, data, status);
--- edatos-external-users-internal-js/webapp/src/app/shared/service/favorite/favorite.service.ts
+++ edatos-external-users-internal-js/webapp/src/app/shared/service/favorite/favorite.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { mapPlainToClass, ResponseWrapper } from '@app/core/utils/response-utils';
+import { convert, ResponseWrapper } from '@app/core/utils/response-utils';
 import { Favorite } from '@app/shared/model/favorite.model';
 import { createRequestOption } from 'arte-ng/utils';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root',
@@ -14,24 +15,21 @@ export class FavoriteService {
     constructor(private http: HttpClient) {}
 
     public get(id: number): Observable<Favorite> {
-        return this.http.get<Favorite>(`${this.resourceUrl}/${id}`).pipe(mapPlainToClass(Favorite));
+        return this.http.get<Favorite>(`${this.resourceUrl}/${id}`).pipe(map((favorite) => convert(Favorite, favorite)));
     }
 
     public find(req?: any): Observable<ResponseWrapper<Favorite[]>> {
         const options = createRequestOption(req);
-        return (
-            this.http
-                .get<Favorite[]>(this.resourceUrl, { ...options, observe: 'response' })
-                // @ts-ignore
-                .pipe(mapPlainToClass(Favorite))
-        );
+        return this.http
+            .get<Favorite[]>(this.resourceUrl, { ...options, observe: 'response' })
+            .pipe(map((favorite) => convert(Favorite, favorite)));
     }
 
     public save(filter: Favorite): Observable<Favorite> {
-        return this.http.post<Favorite>(this.resourceUrl, filter).pipe(mapPlainToClass(Favorite));
+        return this.http.post<Favorite>(this.resourceUrl, filter).pipe(map((favorite) => convert(Favorite, favorite)));
     }
 
     public update(filter: Favorite): Observable<Favorite> {
-        return this.http.put<Favorite>(this.resourceUrl, filter).pipe(mapPlainToClass(Favorite));
+        return this.http.put<Favorite>(this.resourceUrl, filter).pipe(map((favorite) => convert(Favorite, favorite)));
     }
 }
