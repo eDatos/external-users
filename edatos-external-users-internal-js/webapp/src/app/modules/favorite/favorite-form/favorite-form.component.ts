f0654fc2aceaf37088c6fcdde5ed76a75981a868
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-03-29 08:45:18 +0100

    EDATOS-3294 - CRUD favoritos
    > Modifica el campo del email para hacerlo un select autocompletable.
    Pero hay que revisarlo porque su comportamiento es extraño.

--- edatos-external-users-internal-js/webapp/src/app/modules/favorite/favorite-form/favorite-form.component.ts
+++ edatos-external-users-internal-js/webapp/src/app/modules/favorite/favorite-form/favorite-form.component.ts
@@ -16,7 +16,7 @@ export class FavoriteFormComponent implements OnInit {
     public isLoading = false;
     public favorite: Favorite;
     public mainLanguageCode: string;
-    public externalUserEmailsList: string[];
+    public externalUserList: string[];
 
     constructor(
         private favoriteService: FavoriteService,
@@ -74,15 +74,23 @@ export class FavoriteFormComponent implements OnInit {
 
     public updateExternalUserSuggestions(event) {
         this.externalUserService.find({ query: `NAME ILIKE '%${event.query}%' OR EMAIL ILIKE '%${event.query}%'` }).subscribe((results) => {
-            this.externalUserEmailsList = results.body.map((user) => user.email);
+            this.externalUserList = results.body.map((user) => user.email);
         });
     }
 
-    public externalUserTemplate(t): string {
-        if (t instanceof Object && '_ITEM_TEMPLATE_FIELD_' in t) {
-            return t['_ITEM_TEMPLATE_FIELD_'];
+    public setEmail(email) {
+        if (email instanceof Object && '_ITEM_TEMPLATE_FIELD_' in email) {
+            this.favorite.email = email['_ITEM_TEMPLATE_FIELD_'];
+        } else {
+            this.favorite.email = email;
         }
-        return t;
+    }
+
+    public emailTemplate(obj) {
+        if (obj instanceof Object && '_ITEM_TEMPLATE_FIELD_' in obj) {
+            return obj['_ITEM_TEMPLATE_FIELD_'];
+        }
+        return obj;
     }
 
     private toggleIsSaving(): void {
