cc09e050c95c72a7c074ad9868f582bda2fedc4f
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-04-06 14:29:07 +0100

    EDATOS-3278 - Administración de usuarios externos
    > Modifica ExternalUserMapper para que no intente codificar la
    contraseña del usuario externo.

--- edatos-external-users-rest-common/src/main/java/es/gobcan/istac/edatos/external/users/rest/common/mapper/ExternalUserMapper.java
+++ edatos-external-users-rest-common/src/main/java/es/gobcan/istac/edatos/external/users/rest/common/mapper/ExternalUserMapper.java
@@ -2,9 +2,6 @@ package es.gobcan.istac.edatos.external.users.rest.common.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.Named;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import es.gobcan.istac.edatos.external.users.core.domain.ExternalUserEntity;
 import es.gobcan.istac.edatos.external.users.core.repository.ExternalUserRepository;
@@ -18,12 +15,6 @@ public abstract class ExternalUserMapper implements EntityMapper<ExternalUserDto
     public abstract ExternalUserDto toDto(ExternalUserEntity entity);
 
     @Override
-    @Mapping(target = "password", source = "password", qualifiedByName = "encodePassword")
+    @Mapping(target = "password", ignore = true)
     public abstract ExternalUserEntity toEntity(ExternalUserDto dto);
-
-    @Named("encodePassword")
-    public String encodePassword(String unencryptedPassword) {
-        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        return passwordEncoder.encode(unencryptedPassword);
-    }
 }
