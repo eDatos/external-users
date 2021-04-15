688ee352b522efc01e46566e845457fe323ff933
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-04-15 11:28:10 +0100

    EDATOS-3345 - Consultar usuarios registrados
    > Añade una checkbox en el panel de un usuario para activar y desactivar
    el envío de notificaciones por email para usuarios concretos.

--- edatos-external-users-internal-js/webapp/src/app/core/model/external-user.model.ts
+++ edatos-external-users-internal-js/webapp/src/app/core/model/external-user.model.ts
@@ -3,21 +3,18 @@ import { Treatment } from '@app/core/model/treatment.model';
 import { BaseVersionedAndAuditingWithDeletionEntity } from 'arte-ng/model';
 
 export class ExternalUser extends BaseVersionedAndAuditingWithDeletionEntity {
-    constructor(
-        public id?: any,
-        public name?: string,
-        public surname1?: string,
-        public surname2?: string,
-        public password?: string,
-        public email?: string,
-        public organism?: string,
-        public treatment?: Treatment,
-        public language?: Language,
-        public phoneNumber?: string,
-        public roles?: any[]
-    ) {
-        super();
-    }
+    public id?: any;
+    public name?: string;
+    public surname1?: string;
+    public surname2?: string;
+    public password?: string;
+    public email?: string;
+    public organism?: string;
+    public treatment?: Treatment;
+    public language?: Language;
+    public phoneNumber?: string;
+    public emailNotificationsEnabled?: boolean;
+    public roles?: any[];
 
     public getFullName(): string {
         return `${this.name} ${this.surname1} ${this.surname2}`;
