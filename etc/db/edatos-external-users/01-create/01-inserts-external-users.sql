b8c1d83a3211350ac1f53dd4d79086ab5862e4e0
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-04-16 12:56:32 +0100

    EDATOS-3294 - Consulta de favoritos
    > Modifica los correos de los usuarios de ejemplo en los scripts de SQL.

--- etc/db/edatos-external-users/01-create/01-inserts-external-users.sql
+++ etc/db/edatos-external-users/01-create/01-inserts-external-users.sql
@@ -1,11 +1,11 @@
 INSERT INTO tb_external_users (id, opt_lock, email, password, name, surname1, surname2, treatment, language, phone_number, organism, created_by, created_date, email_notifications_enabled)
-VALUES (1, 1, 'eliz@gmail.com', 'password', 'Elizabeth', 'Rodríguez', 'Elvira', 'LADY', 'SPANISH', '612345678', 'Agencia Tributaria', 'system', '2021-04-15 08:19:33.000000', true);
+VALUES (1, 1, 'arte-consultores-elizabeth@test.com', 'password', 'Elizabeth', 'Rodríguez', 'Elvira', 'LADY', 'SPANISH', '612345678', 'Agencia Tributaria', 'system', '2021-04-15 08:19:33.000000', true);
 
 INSERT INTO tb_external_users (id, opt_lock, email, password, name, surname1, surname2, language, created_by, created_date, email_notifications_enabled)
-VALUES (2, 1, 'leocarper@test.com', 'password', 'Leo', 'Carballo', 'Peral', 'CATALAN', 'system', '2021-04-15 08:19:33.000000', true);
+VALUES (2, 1, 'arte-consultores-leo@test.com', 'password', 'Leo', 'Carballo', 'Peral', 'CATALAN', 'system', '2021-04-15 08:19:33.000000', true);
 
 INSERT INTO tb_external_users (id, opt_lock, email, password, name, surname1, surname2, language, created_by, created_date, email_notifications_enabled)
-VALUES (3, 1, 'maria@gmail.com', 'password', 'Maria Julia', 'Chacón', 'Reyes', 'CATALAN', 'system', '2021-04-15 08:19:33.000000', true);
+VALUES (3, 1, 'arte-consultores-maria@test.com', 'password', 'Maria Julia', 'Chacón', 'Reyes', 'CATALAN', 'system', '2021-04-15 08:19:33.000000', true);
 
 INSERT INTO tb_external_users (id, opt_lock, email, password, name, surname1, surname2, treatment, language, created_by, created_date, email_notifications_enabled)
-VALUES (4, 1, 'maria@gmail.com', 'password', 'Luis  Marco', 'Antón', 'Ochoa', 'MISTER', 'SPANISH', 'system', '2021-04-15 08:19:33.000000', true);
+VALUES (4, 1, 'arte-consultores-luis@test.com', 'password', 'Luis  Marco', 'Antón', 'Ochoa', 'MISTER', 'SPANISH', 'system', '2021-04-15 08:19:33.000000', true);
