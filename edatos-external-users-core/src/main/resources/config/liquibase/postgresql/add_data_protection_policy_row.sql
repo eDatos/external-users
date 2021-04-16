b0ef5f1ff2d88255e0280652c6f623373db10afb
Jorge González Cabrera <jorgeglezca98@gmail.com>
2021-04-16 12:58:30 +0100

    EDATOS-3348 - Especificar política de protección de datos personales
    > Arreglando el changeSet que incluía la fila de la política de
    protección de datos.

--- edatos-external-users-core/src/main/resources/config/liquibase/postgresql/add_data_protection_policy_row.sql
+++ edatos-external-users-core/src/main/resources/config/liquibase/postgresql/add_data_protection_policy_row.sql
@@ -1,3 +1,3 @@
 --liquibase formatted sql
 --changeset arte:1618216857047-3
-INSERT INTO public.tb_data_protection_policy (id, created_by, created_date, value) VALUES(0, 'admin', now(), '{"texts": []}');
+INSERT INTO public.tb_data_protection_policy (id, created_by, created_date, value, opt_lock) VALUES(0, 'admin', now(), '{"texts": []}', 0);
