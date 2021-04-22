01a45cccda5216309bdc46c440734f798d4986fd
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-04-22 11:08:36 +0100

    EDATOS-3345 - Consultar usuarios registrados
    > Actualiza los tests de los favoritos para comprobar que el servidor
    agrega y elimina recursivamente los nodos hijos de las categorías.

--- edatos-external-users-internal-rest/src/test/java/es/gobcan/istac/edatos/external/users/rest/internal/resources/FavoriteResourceInternalTest.java
+++ edatos-external-users-internal-rest/src/test/java/es/gobcan/istac/edatos/external/users/rest/internal/resources/FavoriteResourceInternalTest.java
@@ -1,10 +1,7 @@
 package es.gobcan.istac.edatos.external.users.rest.internal.resources;
 
-import static org.hamcrest.Matchers.*;
-import static org.junit.Assert.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -27,18 +24,25 @@ import es.gobcan.istac.edatos.external.users.core.domain.FavoriteEntity;
 import es.gobcan.istac.edatos.external.users.core.domain.OperationEntity;
 import es.gobcan.istac.edatos.external.users.core.domain.enumeration.Language;
 import es.gobcan.istac.edatos.external.users.core.domain.enumeration.Treatment;
-import es.gobcan.istac.edatos.external.users.core.domain.vo.InternationalStringVO;
-import es.gobcan.istac.edatos.external.users.core.domain.vo.LocalisedStringVO;
 import es.gobcan.istac.edatos.external.users.core.errors.ExceptionTranslator;
 import es.gobcan.istac.edatos.external.users.core.repository.CategoryRepository;
 import es.gobcan.istac.edatos.external.users.core.repository.ExternalUserRepository;
 import es.gobcan.istac.edatos.external.users.core.repository.FavoriteRepository;
 import es.gobcan.istac.edatos.external.users.core.repository.OperationRepository;
+import es.gobcan.istac.edatos.external.users.core.service.FavoriteService;
 import es.gobcan.istac.edatos.external.users.rest.common.dto.FavoriteDto;
 import es.gobcan.istac.edatos.external.users.rest.common.mapper.CategoryMapper;
 import es.gobcan.istac.edatos.external.users.rest.common.mapper.ExternalUserAccountMapper;
 import es.gobcan.istac.edatos.external.users.rest.common.mapper.OperationMapper;
 import es.gobcan.istac.edatos.external.users.rest.common.util.TestUtil;
+
+import static es.gobcan.istac.edatos.external.users.util.InternationalStringUtils.createIntStr;
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = EdatosExternalUsersRestTestApp.class)
@@ -53,6 +57,9 @@ public class FavoriteResourceInternalTest {
 
     @Autowired
     private FavoriteRepository favoriteRepository;
+
+    @Autowired
+    private FavoriteService favoriteService;
 
     @Autowired
     private CategoryRepository categoryRepository;
@@ -84,9 +91,15 @@ public class FavoriteResourceInternalTest {
 
     private ExternalUserEntity user2;
 
+    private CategoryEntity category0a;
+    private CategoryEntity category0b;
     private CategoryEntity category1;
+    private CategoryEntity category2a;
+    private CategoryEntity category2b;
 
     private OperationEntity operation1;
+    private OperationEntity operation2;
+    private OperationEntity operation3;
 
     private FavoriteEntity favorite1;
 
@@ -120,68 +133,119 @@ public class FavoriteResourceInternalTest {
         user2.setLanguage(Language.CATALAN);
         externalUserRepository.saveAndFlush(user2);
 
-        LocalisedStringVO locStr = new LocalisedStringVO();
-        locStr.setLabel("Categoría 1");
-        locStr.setLocale(LOCALE);
+        category0a = new CategoryEntity();
+        category0a.setName(createIntStr(LOCALE, "Categoría abuela 1"));
+        category0a.setUri("uri:000a");
+        category0a.setUrn("urn:000a");
+        category0a.setCode("000a");
+        categoryRepository.saveAndFlush(category0a);
 
-        InternationalStringVO intStr = new InternationalStringVO();
-        intStr.addText(locStr);
+        category0b = new CategoryEntity();
+        category0b.setName(createIntStr(LOCALE, "Categoría abuela 2"));
+        category0b.setUri("uri:000b");
+        category0b.setUrn("urn:000b");
+        category0b.setCode("000b");
+        categoryRepository.saveAndFlush(category0b);
 
         category1 = new CategoryEntity();
-        category1.setName(intStr);
-        category1.setUri("uri:000");
-        category1.setUrn("urn:000");
-        category1.setCode("000");
+        category1.setName(createIntStr(LOCALE, "Categoría madre"));
+        category1.setUri("uri:001");
+        category1.setUrn("urn:001");
+        category1.setCode("001");
+        category1.setParent(category0a);
         categoryRepository.saveAndFlush(category1);
 
-        locStr = new LocalisedStringVO();
-        locStr.setLabel("Operation 1");
-        locStr.setLocale(LOCALE);
+        category2a = new CategoryEntity();
+        category2a.setName(createIntStr(LOCALE, "Categoría hija 1"));
+        category2a.setUri("uri:002a");
+        category2a.setUrn("urn:002a");
+        category2a.setCode("002a");
+        category2a.setParent(category1);
+        categoryRepository.saveAndFlush(category2a);
 
-        intStr = new InternationalStringVO();
-        intStr.addText(locStr);
+        category2b = new CategoryEntity();
+        category2b.setName(createIntStr(LOCALE, "Categoría hija 2"));
+        category2b.setUri("uri:002b");
+        category2b.setUrn("urn:002b");
+        category2b.setCode("002b");
+        category2b.setParent(category1);
+        categoryRepository.saveAndFlush(category2b);
 
         operation1 = new OperationEntity();
-        operation1.setName(intStr);
-        operation1.setCode("o:000");
-        operation1.setCategory(category1);
+        operation1.setName(createIntStr(LOCALE, "Operación 1"));
+        operation1.setCode("o:001");
+        operation1.setCategory(category0a);
         operation1.setProcStatus(ProcStatus.INTERNALLY_PUBLISHED);
         operation1.setStatus(Status.PRODUCTION);
         operationRepository.saveAndFlush(operation1);
 
+        operation2 = new OperationEntity();
+        operation2.setName(createIntStr(LOCALE, "Operación 2"));
+        operation2.setCode("o:002");
+        operation2.setCategory(category2b);
+        operation2.setProcStatus(ProcStatus.INTERNALLY_PUBLISHED);
+        operation2.setStatus(Status.PRODUCTION);
+        operationRepository.saveAndFlush(operation2);
+
+        operation3 = new OperationEntity();
+        operation3.setName(createIntStr(LOCALE, "Operación 3"));
+        operation3.setCode("o:003");
+        operation3.setCategory(category2a);
+        operation3.setProcStatus(ProcStatus.INTERNALLY_PUBLISHED);
+        operation3.setStatus(Status.PRODUCTION);
+        operationRepository.saveAndFlush(operation3);
+
         favorite1 = new FavoriteEntity();
         favorite1.setExternalUser(user1);
-        favorite1.setCategory(category1);
+        favorite1.setOperation(operation1);
         favoriteRepository.saveAndFlush(favorite1);
 
         favorite2 = new FavoriteEntity();
         favorite2.setExternalUser(user2);
-        favorite2.setOperation(operation1);
+        favorite2.setOperation(operation2);
         favoriteRepository.saveAndFlush(favorite2);
     }
 
     @Test
     public void testGetFavoriteById() throws Exception {
         this.mockMvc.perform(get(ENDPOINT_URL + "/" + favorite1.getId())).andExpect(status().isOk()).andExpect(jsonPath("$.id", is(favorite1.getId().intValue())))
-                .andExpect(jsonPath("$.category.name.texts[0].label", is(favorite1.getCategory().getName().getLocalisedLabel(LOCALE))));
+                .andExpect(jsonPath("$.operation.name.texts[0].label", is(favorite1.getOperation().getName().getLocalisedLabel(LOCALE))));
     }
 
     @Test
     public void testGetListOfFavorites() throws Exception {
-        this.mockMvc.perform(get(ENDPOINT_URL)).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].category.name.texts[0].label", is(category1.getName().getLocalisedLabel(LOCALE))))
-                .andExpect(jsonPath("$[1].operation.name.texts[0].label", is(operation1.getName().getLocalisedLabel(LOCALE))));
+        this.mockMvc.perform(get(ENDPOINT_URL)).andDo(print()).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(2)))
+                .andExpect(jsonPath("$[0].operation.name.texts[0].label", is(operation1.getName().getLocalisedLabel(LOCALE))))
+                .andExpect(jsonPath("$[1].operation.name.texts[0].label", is(operation2.getName().getLocalisedLabel(LOCALE))));
     }
 
     @Test
     public void testPostFavorite() throws Exception {
         FavoriteDto dto = new FavoriteDto();
         dto.setExternalUser(externalUserAccountMapper.toDto(user1));
-        dto.setOperation(operationMapper.toDto(operation1));
+        dto.setOperation(operationMapper.toDto(operation3));
 
         this.mockMvc.perform(post(ENDPOINT_URL).contentType(TestUtil.APPLICATION_JSON_UTF8).content(TestUtil.convertObjectToJsonBytes(dto))).andExpect(status().isCreated())
-                .andExpect(jsonPath("$.operation.name.texts[0].label", is(operation1.getName().getLocalisedLabel(LOCALE)))).andExpect(jsonPath("$.externalUser.name", is(user1.getName())));
-        assertTrue(favoriteRepository.findAllByExternalUserOrderByCreatedDate(user1).stream().anyMatch(favorite -> favorite.getOperation() != null && favorite.getOperation().equals(operation1)));
+                .andExpect(jsonPath("$.operation.name.texts[0].label", is(operation3.getName().getLocalisedLabel(LOCALE)))).andExpect(jsonPath("$.externalUser.name", is(user1.getName())));
+        assertTrue(favoriteRepository.findByExternalUser(user1).stream().anyMatch(favorite -> favorite.getOperation() != null && favorite.getOperation().equals(operation3)));
+    }
+
+    @Test
+    public void testPostCategoryFavoriteRecursivelyMarksAllChildrenAsFavorite() throws Exception {
+        FavoriteDto dto = new FavoriteDto();
+        dto.setExternalUser(externalUserAccountMapper.toDto(user1));
+        dto.setCategory(categoryMapper.toDto(category0a));
+
+        List<FavoriteEntity> favorites = favoriteRepository.findByExternalUser(user1);
+        assertEquals(1, favorites.size());
+        assertThat(favorites.stream().map(fav -> fav.getCategory() != null ? fav.getCategory() : fav.getOperation()).collect(Collectors.toList()), contains(operation1));
+
+        this.mockMvc.perform(post(ENDPOINT_URL).contentType(TestUtil.APPLICATION_JSON_UTF8).content(TestUtil.convertObjectToJsonBytes(dto))).andExpect(status().isCreated());
+
+        favorites = favoriteRepository.findByExternalUser(user1);
+        assertEquals(7, favorites.size());
+        assertThat(favorites.stream().map(fav -> fav.getCategory() != null ? fav.getCategory() : fav.getOperation()).collect(Collectors.toList()),
+                hasItems(category0a, category1, category2a, category2b, operation1, operation2, operation3));
     }
 
     @Test
@@ -192,11 +256,24 @@ public class FavoriteResourceInternalTest {
     }
 
     @Test
+    public void testDeleteFavoriteRecursivelyDeletesFavoriteChildren() throws Exception {
+        FavoriteEntity entity = new FavoriteEntity();
+        entity.setExternalUser(user1);
+        entity.setCategory(category0a);
+        entity = favoriteService.create(entity);
+        List<FavoriteEntity> favorites = favoriteRepository.findByExternalUser(user1);
+        assertEquals(7, favorites.size());
+        this.mockMvc.perform(delete(ENDPOINT_URL + "/" + entity.getId())).andExpect(status().isNoContent());
+        favorites = favoriteRepository.findByExternalUser(user1);
+        assertEquals(0, favorites.size());
+    }
+
+    @Test
     public void testFavoriteCantHaveOperationAndCategoryAtTheSameTime() throws Exception {
         FavoriteDto dto = new FavoriteDto();
         dto.setExternalUser(externalUserAccountMapper.toDto(user1));
         dto.setOperation(operationMapper.toDto(operation1));
-        dto.setCategory(categoryMapper.toDto(category1));
+        dto.setCategory(categoryMapper.toDto(category0a));
 
         this.mockMvc.perform(post(ENDPOINT_URL).contentType(TestUtil.APPLICATION_JSON_UTF8).content(TestUtil.convertObjectToJsonBytes(dto))).andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.code", equalToIgnoringCase("exception.external_users.favorite.cannot_set_both_operation_and_category")));
@@ -206,7 +283,7 @@ public class FavoriteResourceInternalTest {
     public void testUserCantHaveTheSameFavoriteTwice() throws Exception {
         FavoriteDto dto = new FavoriteDto();
         dto.setExternalUser(externalUserAccountMapper.toDto(user1));
-        dto.setCategory(categoryMapper.toDto(category1));
+        dto.setOperation(operationMapper.toDto(operation1));
 
         this.mockMvc.perform(post(ENDPOINT_URL).contentType(TestUtil.APPLICATION_JSON_UTF8).content(TestUtil.convertObjectToJsonBytes(dto))).andExpect(status().isBadRequest());
     }
@@ -214,7 +291,7 @@ public class FavoriteResourceInternalTest {
     @Test
     public void testGetFavoriteByUser() throws Exception {
         this.mockMvc.perform(get(String.format("%s?query=USER_ID EQ '%s'", ENDPOINT_URL, user1.getId()))).andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)))
-                .andExpect(jsonPath("$[0].category.name.texts[0].label", is(category1.getName().getLocalisedLabel(LOCALE))));
+                .andExpect(jsonPath("$[0].operation.name.texts[0].label", is(operation1.getName().getLocalisedLabel(LOCALE))));
     }
 
     @Test
