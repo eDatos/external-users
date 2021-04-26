40dff697f9fcd94ee8ce049839c60ed32ef66b74
nunigs <ngonsot@arte-consultores.com>
2021-04-26 14:08:23 +0100

    EDATOS-3339 - Gestión de favoritos
    > Actualización del user.service con el ResponseWrapper

--- edatos-external-users-external-js/webapp/src/app/core/service/user/user.service.ts
+++ edatos-external-users-external-js/webapp/src/app/core/service/user/user.service.ts
@@ -4,7 +4,7 @@ import { Credentials, User, UserAccountChangePassword } from '@app/core/model';
 import { map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { createRequestOption, ResponseUtils } from 'arte-ng/utils';
-import { ResponseWrapper } from 'arte-ng/model';
+import { convert, ResponseWrapper } from '@app/core/utils/response-utils';
 
 @Injectable()
 export class AccountUserService {
@@ -13,23 +13,23 @@ export class AccountUserService {
     constructor(private http: HttpClient) {}
 
     create(user: User): Observable<User> {
-        return this.http.post(`${this.resourceUrl}/signup`, user).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.post<User>(`${this.resourceUrl}/signup`, user).pipe(map((res) => convert(User, res)));
     }
 
     buscarUsuarioPorEmail(email: string): Observable<User> {
-        return this.http.get(`${this.resourceUrl}/${email}/findByEmail`).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.get<User>(`${this.resourceUrl}/${email}/findByEmail`).pipe(map((res) => convert(User, res)));
     }
 
     getLogueado(): Observable<User> {
-        return this.http.get(`${this.resourceUrl}`).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.get<User>(`${this.resourceUrl}`).pipe(map((res) => convert(User, res)));
     }
 
     update(user: User): Observable<User> {
-        return this.http.put(`${this.resourceUrl}`, user).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.put<User>(this.resourceUrl, user).pipe(map((res) => convert(User, res)));
     }
 
     login(credentials: Credentials): Observable<User> {
-        return this.http.post(`api/login`, credentials).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.post<User>(`api/login`, credentials).pipe(map((res) => convert(User, res)));
     }
 
     changeCurrentUserPassword(accountChangePassword: UserAccountChangePassword): Observable<User> {
@@ -37,22 +37,17 @@ export class AccountUserService {
     }
 
     delete(id: number): Observable<User> {
-        return this.http.delete(`${this.resourceUrl}/${id}`).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.delete<User>(`${this.resourceUrl}/${id}`).pipe(map((res) => convert(User, res)));
     }
 
-    find(req?: any): Observable<ResponseWrapper> {
+    find(req?: any): Observable<ResponseWrapper<User>> {
         const options = createRequestOption(req);
-        return this.http.get(this.resourceUrl, { ...options, observe: 'response' }).pipe(map((res) => ResponseUtils.convertToResponseWrapper(res, User)));
+        return this.http
+            .get<User>(this.resourceUrl, { ...options, observe: 'response' })
+            .pipe(map((res) => convert(User, res)));
     }
 
     get(id: number): Observable<User> {
-        const options = createRequestOption(true);
-        return this.http.get(`${this.resourceUrl}/${id}`, options).pipe(map((res) => ResponseUtils.convert(res, User)));
+        return this.http.get<User>(`${this.resourceUrl}/${id}`).pipe(map((res) => convert(User, res)));
     }
-
-    /*     
-    restore(login: string): Observable<User> {
-        return this.http.put(`${this.resourceUrl}/${login}/restore`, null).pipe(map((res) => ResponseUtils.convert(res, User)));
-    }
-    */
 }
