8a11366187305c454ea5f17a0763f5eba65c6cde
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-04-30 13:56:44 +0100

    EDATOS-3341 - Consulta de política de protección de datos personales
    > Modifica el SCSS de multi-language-input.component para usar grid.

--- edatos-external-users-internal-js/webapp/src/app/shared/components/multi-language-input/multi-language-input.module.ts
+++ edatos-external-users-internal-js/webapp/src/app/shared/components/multi-language-input/multi-language-input.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { MultiLanguageInputComponent } from '@app/shared/components/multi-language-input/multi-language-input.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { ArteInputTextModule } from 'arte-ng';
+import { InputTextModule } from 'primeng/inputtext';
 
 @NgModule({
     declarations: [MultiLanguageInputComponent],
-    imports: [CommonModule, TranslateModule, FormsModule, ArteInputTextModule],
+    imports: [CommonModule, TranslateModule, FormsModule, ArteInputTextModule, InputTextModule],
     exports: [MultiLanguageInputComponent],
 })
 export class MultiLanguageInputModule {}
