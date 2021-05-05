92f6110738fbb1240329b6a06f8cc7b67c6f4da5
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-05-05 13:46:51 +0100

    EDATOS-3341 - Consulta de política de protección de datos personales
    > Añade equals y hashCode a InternationalStringVO y LocalisedStringVO
    para que Hibernate no detecte esas clases como dirty pese a no haberlas
    modificado.

--- edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/domain/vo/InternationalStringVO.java
+++ edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/domain/vo/InternationalStringVO.java
@@ -2,14 +2,12 @@ package es.gobcan.istac.edatos.external.users.core.domain.vo;
 
 import java.io.Serializable;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class InternationalStringVO implements Serializable {
 
     private final Set<LocalisedStringVO> texts = new HashSet<>();
-
-    public InternationalStringVO() {
-    }
 
     public Set<LocalisedStringVO> getTexts() {
         return texts;
@@ -49,6 +47,23 @@ public class InternationalStringVO implements Serializable {
             }
         }
         return null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof InternationalStringVO)) {
+            return false;
+        }
+        InternationalStringVO that = (InternationalStringVO) o;
+        return Objects.equals(texts, that.texts);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(texts);
     }
 
     @Override
