2734da6c394795412c7924f7a08268c2ba22cd38
nunigs <ngonsot@arte-consultores.com>
2021-05-06 14:58:13 +0200

    EDATOS-3339 Gestión de favoritos
    > se modifica el script de inserción de filtros

--- etc/db/edatos-external-users/01-create/04-inserts-external-users-filters.sql
+++ etc/db/edatos-external-users/01-create/04-inserts-external-users-filters.sql
@@ -1,35 +1,35 @@
-INSERT INTO public.tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
-                               last_access_date, name, notes, permalink, external_user_fk)
-VALUES (1, 'arte-consultores-elizabeth@test.com', '2021-05-06 11:10:17.234000', 'ngonsot@arte-consultores.com',
+INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
+                        last_access_date, name, notes, permalink, external_user_fk)
+VALUES (1, 'system', '2021-05-06 11:10:17.234000', 'system',
         '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000', 'permalink', 'permalink',
         'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 1);
 
-INSERT INTO public.tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
-                               last_access_date, name, notes, permalink, external_user_fk)
-VALUES (2, 'arte-consultores-elizabeth@test.com', '2021-05-06 11:30:54.247000', 'ngonsot@arte-consultores.com',
+INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
+                        last_access_date, name, notes, permalink, external_user_fk)
+VALUES (2, 'system', '2021-05-06 11:30:54.247000', 'system',
         '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'perma43', 'texto con link',
         'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 1);
-INSERT INTO public.tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
-                               last_access_date, name, notes, permalink, external_user_fk)
-VALUES (3, 'arte-consultores-leo@test.com', '2021-05-06 11:10:17.234000', 'ngonsot@arte-consultores.com',
+INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
+                        last_access_date, name, notes, permalink, external_user_fk)
+VALUES (3, 'system', '2021-05-06 11:10:17.234000', 'system',
         '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000', 'permalink', 'permalink',
-        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 1);
+        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 2);
 
-INSERT INTO public.tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
-                               last_access_date, name, notes, permalink, external_user_fk)
-VALUES (4, 'arte-consultores-leo@test.com', '2021-05-06 11:30:54.247000', 'ngonsot@arte-consultores.com',
+INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
+                        last_access_date, name, notes, permalink, external_user_fk)
+VALUES (4, 'system', '2021-05-06 11:30:54.247000', 'system',
         '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'perma43', 'texto con link',
-        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 1);
+        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 2);
 
-INSERT INTO public.tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
-                               last_access_date, name, notes, permalink, external_user_fk)
-VALUES (5, 'arte-consultores-maria@test.com', '2021-05-06 11:10:17.234000', 'ngonsot@arte-consultores.com',
+INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
+                        last_access_date, name, notes, permalink, external_user_fk)
+VALUES (5, 'system', '2021-05-06 11:10:17.234000', 'system',
         '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000', 'permalink', 'permalink',
-        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 1);
+        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 52);
 
-INSERT INTO public.tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
-                               last_access_date, name, notes, permalink, external_user_fk)
-VALUES (6, 'arte-consultores-maria@test.com', '2021-05-06 11:30:54.247000', 'ngonsot@arte-consultores.com',
+INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
+                        last_access_date, name, notes, permalink, external_user_fk)
+VALUES (6, 'system', '2021-05-06 11:30:54.247000', 'system',
         '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'perma43', 'texto con link',
-        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 1);
+        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 52);
 
