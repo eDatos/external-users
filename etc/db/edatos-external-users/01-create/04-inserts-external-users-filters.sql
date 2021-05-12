b45ef0d8374000b8bd2b814628797ff405f3b191
nunigs <ngonsot@arte-consultores.com>
2021-05-12 08:10:39 +0200

    EDATOS-3339 Gestión de favoritos
    > Mejora de los datos para el script de filtros

--- etc/db/edatos-external-users/01-create/04-inserts-external-users-filters.sql
+++ etc/db/edatos-external-users/01-create/04-inserts-external-users-filters.sql
@@ -1,35 +1,42 @@
 INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
                         last_access_date, name, notes, permalink, external_user_fk)
 VALUES (1, 'system', '2021-05-06 11:10:17.234000', 'system',
-        '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000', 'permalink', 'permalink',
-        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 1);
+        '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000',
+        'Indicador referido al número de visitantes que llegan a las Islas Canarias',
+        'Indicador referido al número de visitantes que llegan a las Islas Canarias',
+        't25zbzh9nfmcytr19vzt523gd', 1);
 
 INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
                         last_access_date, name, notes, permalink, external_user_fk)
 VALUES (2, 'system', '2021-05-06 11:30:54.247000', 'system',
-        '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'permalink', 'notas',
-        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 1);
+        '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'Visitantes en las islas',
+        'El número de visitantes en las islas',
+        '1ofnp6ihvgdv8qx11d8t96092m', 1);
 INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
                         last_access_date, name, notes, permalink, external_user_fk)
 VALUES (3, 'system', '2021-05-06 11:10:17.234000', 'system',
-        '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000', 'permalink', 'permalink',
-        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 2);
+        '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000',
+        'Índice censal de ocupación según indicadores por islas, provincias y comunidades', 'índice censal',
+        'ekfnsqu5ue31woq1vmwhev9xn', 2);
 
 INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
                         last_access_date, name, notes, permalink, external_user_fk)
 VALUES (4, 'system', '2021-05-06 11:30:54.247000', 'system',
-        '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'permalink', 'notas',
-        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 2);
+        '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'Visitantes en las islas',
+        'El número de visitantes en las islas',
+        '1ofnp6ihvgdv8qx11d8t96092m', 2);
 
 INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
                         last_access_date, name, notes, permalink, external_user_fk)
 VALUES (5, 'system', '2021-05-06 11:10:17.234000', 'system',
-        '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000', 'permalink', 'permalink',
-        'https://visualizer/data.html?permalink=vr0oanlyid8rvs613sya4ysbi', 52);
+        '2021-05-06 11:23:54.257000', 2, '2021-05-06 11:10:17.219000',
+        'Índice censal de ocupación según indicadores por islas, provincias y comunidades', 'índice censal',
+        'ekfnsqu5ue31woq1vmwhev9xn', 52);
 
 INSERT INTO tb_filters (id, created_by, created_date, last_modified_by, last_modified_date, opt_lock,
                         last_access_date, name, notes, permalink, external_user_fk)
 VALUES (6, 'system', '2021-05-06 11:30:54.247000', 'system',
-        '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'permalink', 'notas',
-        'https://visualizer/data.html?permalink=1iehlgx1d7od76vr11r31d11j5o', 52);
+        '2021-05-06 11:30:54.247000', 0, '2021-05-06 11:30:53.461000', 'Visitantes en las islas',
+        'El número de visitantes en las islas',
+        '1ofnp6ihvgdv8qx11d8t96092m', 52);
 
