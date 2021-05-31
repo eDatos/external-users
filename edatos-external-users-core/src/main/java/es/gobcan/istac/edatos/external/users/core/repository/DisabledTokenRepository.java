a1d5ee77035704fa71bb74eb987c0cf7bdda6d81
Jorge González Cabrera <jorgeglezca98@gmail.com>
2021-05-31 14:42:56 +0100

    EDATOS-3342 - e-Visualiza - Permitir autenticarse
    > Eliminando de la tabla los tokens deshabilitados cuya fecha de
    expiración ya ha pasado.

--- edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/repository/DisabledTokenRepository.java
+++ edatos-external-users-core/src/main/java/es/gobcan/istac/edatos/external/users/core/repository/DisabledTokenRepository.java
@@ -1,5 +1,7 @@
 package es.gobcan.istac.edatos.external.users.core.repository;
 
+import java.time.Instant;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import es.gobcan.istac.edatos.external.users.core.domain.DisabledTokenEntity;
 @Repository
 public interface DisabledTokenRepository extends JpaRepository<DisabledTokenEntity, String> {
 
+    public void deleteByExpirationDateBefore(Instant date);
 }
