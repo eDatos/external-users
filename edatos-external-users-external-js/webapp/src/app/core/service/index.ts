62d18f4c04714a246a1d000e354f2a108aeeb9f6
Francisco Arturo Cruz Zelante <fcruzel@arte-consultores.com>
2021-08-20 07:30:10 +0100

    EDATOS-3349 - Consulta de incidencias
    > Elimina la consulta y el envío de filtros de eUsuarios.

--- edatos-external-users-external-js/webapp/src/app/core/service/index.ts
+++ edatos-external-users-external-js/webapp/src/app/core/service/index.ts
@@ -2,6 +2,5 @@ export * from './auth';
 export * from './log';
 export * from './profile';
 export * from './language';
-export * from './issues';
 export * from './alert/eusuarios-alert.service';
 export * from './user';
